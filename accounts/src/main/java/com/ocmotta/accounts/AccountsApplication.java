1a6097d853572a0ac28d9393a15d782d8eccf078
Oruam Catani <oru_catani@hotmail.com>
2025-07-03 14:28:53 -0300

    feat: documentation operation and apiresponses

--- accounts/src/main/java/com/ocmotta/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/ocmotta/accounts/AccountsApplication.java
@@ -1,31 +1,36 @@
 package com.ocmotta.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-//@OpenAPIDefinition(
-//        info = @Info(
-//                title = "Accounts microservice REST API Documentation",
-//                description = "EazyBank Accounts microservice REST API documentation",
-//                version = "v1",
-//                contact = @Contact(
-//                        name = "Oruam Catani",
-//                        email = "email@email.com",
-//                        url = "http://localhost:8080"
-//                ),
-//                license = @License(
-//                        name = "Apache 2.0",
-//                        url = "http://localhost:8080/licenses/LICENSE-2.0.html"
-//                )
-//        ),
-//        externalDocs = @ExternalDocumentation(
-//                description = "EazyBank Accounts microservice REST API Documentation",
-//                url = "http://localhost:8080/docs"
-//        )
-//)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Oruam Catani",
+                        email = "email@email.com",
+                        url = "http://localhost:8080"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://localhost:8080/licenses/LICENSE-2.0.html"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "http://localhost:8080/docs"
+        )
+)
 public class AccountsApplication {
 
     public static void main(String[] args) {
--- accounts/src/main/java/com/ocmotta/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/ocmotta/accounts/controller/AccountsController.java
@@ -1,8 +1,14 @@
 package com.ocmotta.accounts.controller;
 
 import com.ocmotta.accounts.dto.CustomerDto;
+import com.ocmotta.accounts.dto.ErrorResponseDto;
 import com.ocmotta.accounts.dto.ResponseDto;
 import com.ocmotta.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
@@ -35,6 +41,24 @@ public class AccountsController {
      * @param customerDto the customer details
      * @return ResponseEntity with status 201 Created and a response message
      */
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer &  Account inside EazyBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
@@ -49,6 +73,24 @@ public class AccountsController {
      * @param mobileNumber the customer's mobile number
      * @return ResponseEntity with the customer's account details
      */
+    @Operation(
+            summary = "Fetch Account Details REST API",
+            description = "REST API to fetch Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(
             @RequestParam(name = "mobileNumber")
@@ -66,6 +108,28 @@ public class AccountsController {
      * @param customerDto the updated customer details
      * @return ResponseEntity with status 200 OK or 500 Internal Server Error
      */
+    @Operation(
+            summary = "Update Account Details REST API",
+            description = "REST API to update Customer &  Account details based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -86,6 +150,28 @@ public class AccountsController {
      * @param mobileNumber the customer's mobile number
      * @return ResponseEntity with status 200 OK or 500 Internal Server Error
      */
+    @Operation(
+            summary = "Delete Account & Customer Details REST API",
+            description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccount(
             @RequestParam(name = "mobileNumber")
